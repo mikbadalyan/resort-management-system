e70595c6911e9ee81484f58da54e9009b7e2df98
Mikayel <mikayel.b.a@gmail.com>
2025-04-11 01:26:24 +0400

    Initial Commit

---
+++ resort-management-system/src/main/java/com/resort/managementsystem/ResortManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.resort.managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResortManagementSystemApplication.class, args);
+    }
+
+}
---
+++ resort-management-system/src/test/java/com/resort/managementsystem/ResortManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resort.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
